e41e1bf35c9b1f8ba9f4c4ce405ba05b4e708515
Stefano <stefano.miscioscia.02@gmail.com>
2026-02-27 15:54:13 +0100

    Aggiunta emtodo per mescolare risposte della domanda proposta

--- Domanda.py
+++ Domanda.py
@@ -5,3 +5,9 @@ class Domanda:
        self.lvl_difficolta = lvl_difficolta # riga 2
        self.corretta = corretta # riga 3
        self.sbagliata = sbagliata # riga 4 in poi
+
+    def genera_opzioni_mescolate(self):
+        # Restituisce una lista di risposte (corretta + sbagliate) mescolate
+        risposte = [self.corretta] + self.sbagliata
+        random.shuffle(risposte)
+        return risposte
