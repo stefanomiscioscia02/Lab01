c7d7136ee4b25344abd14d07174a56a7a96b46f6
Stefano <stefano.miscioscia.02@gmail.com>
2026-02-28 20:04:27 +0100

    inizio gestione classifica: al momento ho solo letto il file punti.txt e creato una classifica

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Domanda import Domanda
-
+from Giocatore import Giocatore
 import random
 
 def crea_lista_domande(nomefile):
@@ -16,16 +16,33 @@ def crea_lista_domande(nomefile):
             lista_domande.append(dTemp)
     return lista_domande
 
+def gestione_classifica(nomefile):
+    classifica = []
+    with open(nomefile, "r", encoding = 'utf-8') as file:
+        parti = [line.strip().split() for line in file if line.strip().split()]
+        for p in parti:
+            if len(p) >= 2:
+                nick = p[0]
+                punti = p[1]
+                classifica.append((nick, punti))
+    #print(f"DEBUG Classifica: {classifica}")  # <--- TEST 2
+    return classifica
+
+
 def main():
     lista_domande_gioco = crea_lista_domande("domande.txt")
     if not lista_domande_gioco:
         print("Non ci sono domande nel file. Impossibile giocare.")
         return
 
+    # Creazione di variabili utili per gli if e iterazioni
     lvl_attuale = 0
+    # cerco lvl_max per gestire poi gli if
     lvl_max = max(d.lvl_difficolta for d in lista_domande_gioco)
     punteggio = 0
     stato_del_gioco = True
+
+    # -GESTIONE del gioco
     print("----- TRIVIA GAME -----")
     while stato_del_gioco:
         domande_per_livello = [d for d in lista_domande_gioco if d.lvl_difficolta == lvl_attuale]
@@ -40,14 +57,19 @@ def main():
         if opzione_inserita == domanda_proposta.corretta:
             print("La risposta è corretta!")
             if lvl_attuale == lvl_max:
-                print(f"Bravo! Hai concluso il gioco. Hai totalizzato un punteggio di: {punteggio}")
+                print(f"Bravo! Hai risposto corettamente a tutte le domande! Il gioco è concluso.")
                 stato_del_gioco = False
             else:
                 lvl_attuale += 1
                 punteggio += 1
         else:
-            print(f"Hai sbagliato! la risposta corretta era: {domanda_proposta.corretta}.\nHai totalizzato un punteggio di: {punteggio}")
+            print(f"Hai sbagliato! la risposta corretta era: {domanda_proposta.corretta}.")
             stato_del_gioco = False
 
+    # -GESTIONE Giocatore e classifica
+    print(f"Hai totalizzato un punteggio di: {punteggio}")
+    nickaname = input("Inserire il tuo Nickname:")
 
-main()
+
+#main()
+gestione_classifica('punti.txt')
